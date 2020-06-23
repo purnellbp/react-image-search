ea3d42b939c30bce569447cb987607756a6f4773
Brian Purnell <purnellbp@gmail.com>
2020-06-23 11:48:55 -0500

    Added splash api using axios

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,31 @@
 import React from "react";
+import axios from 'axios';
+import SearchBar from "./SearchBar";
 
-const App = () => {
-    return (
-        <div>App</div>
-    );
+class App extends React.Component {
+    state = {images: []};
 
-};
+    onSearchSubmit = async term => {
+        const response = await axios.get('https://api.unsplash.com/search/photos', {
+            params: {query: term},
+            headers: {
+                Authorization: 'Client-ID 9NJpPzM97EUFPMoq-_mZa1mYvwIcI29_avm9D5pfjSo'
+            }
+        });
+        this.setState({images: response.data.results});
+    }
+
+    render() {
+        return (
+            <div className="ui container" style={{marginTop: '10px'}}>
+                <SearchBar onSubmit={this.onSearchSubmit}/>
+                Found: {this.state.images.length} images
+            </div>
+        );
+    }
+}
 
 export default App;
+
+
+// 9NJpPzM97EUFPMoq-_mZa1mYvwIcI29_avm9D5pfjSo
