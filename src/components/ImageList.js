6612c665475bebf3704fac8452111f53987efe45
Brian Purnell <purnellbp@gmail.com>
2020-06-24 16:32:37 -0500

    adding image list modifiers and css

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,13 +1,14 @@
+import './ImgaeList.css';
 import React from "react";
 
 const ImageList = props => {
     const images = props.images.map(({description, id, urls}) => {
         return (
             <img key={id} alt={description} src={urls.regular}/>
-            );
+        );
     });
 
-    return <div>{images}</div>
+    return <div className="image-list">{images}</div>
 };
 
 export default ImageList;
